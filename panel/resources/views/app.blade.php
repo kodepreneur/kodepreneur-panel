4527df8040f6e109496de46376852bd34b126b1c
kodepreneur <kodepreneur.official@gmail.com>
2026-08-28 10:04:35 +0700

    fix(view): remove unused @routes directive in app.blade.php

--- panel/resources/views/app.blade.php
+++ panel/resources/views/app.blade.php
@@ -11,7 +11,6 @@
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=JetBrains+Mono:wght@400;500;600&display=swap" rel="stylesheet">
 
         <!-- Scripts & Styles -->
-        @routes
         @vite(['resources/css/app.css', 'resources/js/app.ts'])
         @inertiaHead
     </head>
